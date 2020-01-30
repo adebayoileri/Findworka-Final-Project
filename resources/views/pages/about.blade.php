b25e2a7184721c6525b307b81272c7b5843ddd96
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-01-30 16:19:32 +0100

    Added about page content

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,5 +3,7 @@
 @section('content')
 <div class="container">
     <h1>welcome to our about page</h1>
+    <p>Africa’s youth population is expected to double to over 830 million by 2050. Youth unemployment is still high and the numbers are scary. This project will be driven by the Findworka academy. Our approach will be highly market-driven with a focus on immediate employability upon graduation. Our regular courses are Front-end development, Back-end development, Data science and machine learning, Back-end development, Mobile app development, DevOps and Project Management. For this project, we will add data entry to increase our impact opportunity. Our plan to maintain our impact-gains after this empowerment project is to immediately connect the graduates with internships and entry level jobs with high-growth businesses and brilliant founders upon graduation and maintain a support structure primarily through our talent outsourcing efforts and our Saucecode tech community.
+    </p>
 </div>
 @endsection
