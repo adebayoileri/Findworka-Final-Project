c549b09adbd502fd5a7289c7896a377f653be0b8
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-01-31 14:33:31 +0100

    Passed in middleware to ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //  public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+     public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
