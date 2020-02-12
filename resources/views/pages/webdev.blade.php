e3b8b513aefbc6446b212044fe5396791dc4fed0
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-02-12 09:46:48 +0100

    Course design frontend

--- resources/views/pages/webdev.blade.php
+++ resources/views/pages/webdev.blade.php
@@ -1,39 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Web Development</h1>
+<div class="hero-area section">
 
-<!-- Large modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Frontend Development</button>
+    <!-- Backgound Image -->
+    <div class="bg-image bg-parallax overlay" style="background-image:url('https://res.cloudinary.com/springboard-images/image/upload/q_auto,f_auto,fl_lossy/wordpress/2019/07/sb-blog-programming.png')"></div>
+    <!-- /Backgound Image -->
+<div class="row">
+    <div class="col-md-10 col-md-offset-1 text-center">
+        <h1 class="white-text">Web Development</h1>
 
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-        Mobile Development encompasses the creation of apps for use on devices such as tablets, smartphones, automobiles and watches. Mobile apps are designed and built for different operating systems. The Android operating system and Apple’s iOS are the most popular.
     </div>
-  </div>
 </div>
-
-<!-- Large modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Backend Development</button>
-
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-        Mobile Development encompasses the creation of apps for use on devices such as tablets, smartphones, automobiles and watches. Mobile apps are designed and built for different operating systems. The Android operating system and Apple’s iOS are the most popular.
-    </div>
-  </div>
 </div>
-
-<!-- Large modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">FullStack Development</button>
-
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-        Mobile Development encompasses the creation of apps for use on devices such as tablets, smartphones, automobiles and watches. Mobile apps are designed and built for different operating systems. The Android operating system and Apple’s iOS are the most popular.
-    </div>
-  </div>
+<div class="container">
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content black-text">
+              Mobile Development encompasses the creation of apps for use on devices such as tablets, smartphones, automobiles and watches. Mobile apps are designed and built for different operating systems. The Android operating system and Apple’s iOS are the most popular.
+          </div>
+            </div>
+        </div>
+     </div>
+    </div>      
 </div>
-
 @endsection
